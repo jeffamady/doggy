74a6b437d6bd89905990f140df051f885694a2f0
Jeff Amady <jeff02.jn@gmail.com>
2022-03-15 18:35:53 -0300

    splash screen done

---
+++ app/src/main/java/com/amadydev/doggy/DoggyApp.kt
@@ -0,0 +1,25 @@
+package com.amadydev.doggy
+
+import android.app.Application
+import cat.ereza.customactivityoncrash.config.CaocConfig
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DoggyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        crashConfig()
+    }
+
+
+    private fun crashConfig() {
+        CaocConfig.Builder.create()
+            .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT)
+            .showErrorDetails(false)
+            .minTimeBetweenCrashesMs(2000)
+            .logErrorOnRestart(false)
+            .errorDrawable(R.mipmap.ic_launcher)
+            .restartActivity(MainActivity::class.java)
+            .apply()
+    }
+}
---
+++ app/src/main/java/com/amadydev/doggy/ui/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.amadydev.doggy.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.amadydev.doggy.MainActivity
+import com.amadydev.doggy.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 3000)
+    }
+}
