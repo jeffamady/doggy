f527aff68473bf51b49f62081e62bc86f3f1357d
Jeff Amady <jeff02.jn@gmail.com>
2022-03-18 12:22:45 -0300

    click back to exit

--- app/src/main/java/com/amadydev/doggy/ui/main/MainActivity.kt
+++ app/src/main/java/com/amadydev/doggy/ui/main/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.amadydev.doggy.ui.main
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private var clickBack = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +45,28 @@ class MainActivity : AppCompatActivity() {
     fun setActionBarTitle(title: String = getString(R.string.app_name)) {
         binding.tvToolbar.text = title
     }
+
+    private fun clickBackToExit() {
+        if (clickBack) {
+            super.onBackPressed()
+            return
+        }
+
+        clickBack = true
+
+        Toast.makeText(
+            this,
+            getString(R.string.please_click_back),
+            Toast.LENGTH_SHORT
+        ).show()
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            { clickBack = false }, 2000
+        )
+    }
+
+    override fun onBackPressed() =
+        clickBackToExit()
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
